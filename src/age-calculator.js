fe3c43bcae58eea87cd84a6ec83aceecc2dc5a99
William Pratt <willpratt78@gmail.com>
2022-04-15 11:24:30 -0700

    pass for return age rounded mercuryAge

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,9 +1,11 @@
 export class AgeCalculator {
   constructor(age) {
     this.age = age
-  }
+  };
 
-  mercuryAge(input) {
-    
-  }
+  mercuryAge(age) {
+    this.fullMercuryAge = this.age * (1 / .24);
+    this.mercuryAgeRounded = Math.round(this.fullMercuryAge * 10) / 10;
+    return this.mercuryAgeRounded;
+  };
 }
