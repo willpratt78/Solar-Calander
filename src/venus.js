1c01f328da4ba51b793290dce17e76628b730574
William Pratt <willpratt78@gmail.com>
2022-04-15 09:57:25 -0700

    dry up code

--- src/venus.js
+++ src/venus.js
@@ -1,12 +1,12 @@
 export class Venus {
   constructor(input) {
     this.input = input;
+    this.adjustedAge;
   }
 
   adjustedAge(input) {
     this.fullVenusAge = this.input * (1 / .62);
     this.venusAge = Math.round(this.fullVenusAge * 10) / 10;
-    console.log(this.venusAge)
     return this.venusAge;
   }
 }
