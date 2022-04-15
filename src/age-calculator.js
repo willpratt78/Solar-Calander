2c827e1eb004e00933f21c6d22b12ac11b5f790a
William Pratt <willpratt78@gmail.com>
2022-04-15 16:15:55 -0700

    added ; where needed

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -14,37 +14,36 @@ export class AgeCalculator {
     this.fullVenusAge = this.age * (1 / .62);
     this.venusAgeRounded = Math.round(this.fullVenusAge * 10) / 10;
     return this.venusAgeRounded;
-  }
+  };
 
   marsAge(age) {
     this.fullMarsAge = this.age * (1 / 1.88);
     this.marsAgeRounded = Math.round(this.fullMarsAge * 10) / 10;
     return this.marsAgeRounded;
-  }
+  };
 
   jupiterAge(age) {
     this.fullJupiterAge = this.age * (1 / 11.86);
     this.jupiterAgeRounded = Math.round(this.fullJupiterAge * 10) / 10;
     return this.jupiterAgeRounded;
-  }
+  };
 
   lifeExpectancyMercury(age, lifeExpectancy) {
     this.mercuryAge();
     this.lifeExpectancy = Math.abs(this.mercuryAge());
-  
-  }
+  };
   lifeExpectancyVenus(age, lifeExpectancy) {
     this.venusAge();
     this.lifeExpectancy = Math.abs(this.venusAge());
-  
-  }
+  };
+
   lifeExpectancyMars(age, lifeExpectancy) {
     this.marsAge();
     this.lifeExpectancy = Math.abs(this.marsAge());
-  }
+  };
 
   lifeExpectancyJupiter(age, lifeExpectancy) {
     this.jupiterAge();
     this.lifeExpectancy = Math.abs(this.jupiterAge());
-  }
-}
+  };
+};
