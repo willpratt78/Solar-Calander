af00b5ee3b6c5ae4b84324adb52f5cc02b0040d0
William Pratt <willpratt78@gmail.com>
2022-04-15 16:30:55 -0700

    update index.html, add start to UI in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import { AgeCalculator } from "./age-calculator.js";
+
+
+
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$("user-info").on( "click", function (event) {
+  event.preventDefault();
+  let inputtedAge = $("#age").val();
+  let inputtedExpectancy = $("life-expectency").val();
+  let info = new AgeCalculator(inputtedAge, inputtedExpectancy);
+  console.log(info)
+});
