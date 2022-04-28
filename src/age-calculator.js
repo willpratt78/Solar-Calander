e502a7ae75e8083a2e0d8da3d1c6af78a33ec852
William Pratt <willpratt78@gmail.com>
2022-04-28 16:24:52 -0700

    removed errors from npm run build

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -2,48 +2,49 @@ export class AgeCalculator {
   constructor(age, lifeExpectancy) {
     this.age = age;
     this.lifeExpectancy = lifeExpectancy;
-  };
+  }
 
-  mercuryAge(age) {
+  mercuryAge() {
     this.fullMercuryAge = this.age * (1 / .24);
     this.mercuryAgeRounded = Math.round(this.fullMercuryAge * 10) / 10;
     return this.mercuryAgeRounded;
-  };
+  }
 
-  venusAge(age) {
+  venusAge() {
     this.fullVenusAge = this.age * (1 / .62);
     this.venusAgeRounded = Math.round(this.fullVenusAge * 10) / 10;
     return this.venusAgeRounded;
-  };
+  }
 
-  marsAge(age) {
+  marsAge() {
     this.fullMarsAge = this.age * (1 / 1.88);
     this.marsAgeRounded = Math.round(this.fullMarsAge * 10) / 10;
     return this.marsAgeRounded;
-  };
+  }
 
-  jupiterAge(age) {
+  jupiterAge() {
     this.fullJupiterAge = this.age * (1 / 11.86);
     this.jupiterAgeRounded = Math.round(this.fullJupiterAge * 10) / 10;
     return this.jupiterAgeRounded;
-  };
+  }
 
-  lifeExpectancyMercury(age, lifeExpectancy) {
+  lifeExpectancyMercury() {
     this.mercuryAge();
     this.lifeExpectancy = Math.abs(this.mercuryAge());
-  };
-  lifeExpectancyVenus(age, lifeExpectancy) {
+  }
+
+  lifeExpectancyVenus() {
     this.venusAge();
     this.lifeExpectancy = Math.abs(this.venusAge());
-  };
+  }
 
-  lifeExpectancyMars(age, lifeExpectancy) {
+  lifeExpectancyMars() {
     this.marsAge();
     this.lifeExpectancy = Math.abs(this.marsAge());
-  };
+  }
 
-  lifeExpectancyJupiter(age, lifeExpectancy) {
+  lifeExpectancyJupiter() {
     this.jupiterAge();
     this.lifeExpectancy = Math.abs(this.jupiterAge());
-  };
-};
+  }
+}
